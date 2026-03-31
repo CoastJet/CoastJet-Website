65d44444a577a002b826ce575463cddd9e8db0c5
Saturn Aerospace <131621292+CoastJet@users.noreply.github.com>
2026-03-31 18:59:38 +0100

    Initial Test Version

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+const header = document.getElementById("header");
 
+let lastState = false;
+
+window.addEventListener("scroll", () => {
+    const isScrolled = window.scrollY > 50;
+
+    if (isScrolled !== lastState) {
+        header.classList.toggle("scrolled", isScrolled);
+        lastState = isScrolled;
+    }
+});
